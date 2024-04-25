1d8b8bcc53db2347d96c19a04b36b5938a12e345
Walber Vaz <wvs.walber@gmail.com>
2024-04-25 13:53:49 -0300

    chore: initial project

---
+++ tests/test_main.py
@@ -0,0 +1,14 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get('/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'Hello World'}
