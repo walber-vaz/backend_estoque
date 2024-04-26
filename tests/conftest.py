5c90e48387730574e46987ff78048a9bbbc33270
Walber Vaz <wvs.walber@gmail.com>
2024-04-25 21:38:47 -0300

    feat: set pytest asyncio

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,10 @@
-import pytest
+import pytest_asyncio
 from httpx import AsyncClient
 
 from app.main import app
 
 
-@pytest.fixture()
+@pytest_asyncio.fixture
 async def client():
     async with AsyncClient(app=app, base_url='http://test') as client:
         yield client
