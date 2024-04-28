a7d8cf2ba05d422cfb56e58a2a9e8cdf42a10a14
Walber Vaz <wvs.walber@gmail.com>
2024-04-28 15:27:55 -0300

    feat: create schema response

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from app.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def hello_world():
     return {'message': 'Hello, World!'}
